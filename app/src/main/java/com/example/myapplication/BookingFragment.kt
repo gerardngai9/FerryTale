8fb275a2b0af39ac937d1aa52d47bc834e28fb97
gerardngai9 <gerardngai9@gmail.com>
2020-04-19 04:17:52 +0800

    First commit

--- app/src/main/java/com/example/myapplication/BookingFragment.kt
+++ app/src/main/java/com/example/myapplication/BookingFragment.kt
@@ -7,7 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.myapplication.databinding.FragmentBookingBinding
+import kotlinx.android.synthetic.main.fragment_booking.view.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -22,7 +25,12 @@ class BookingFragment : Fragment() {
             inflater,
             R.layout.fragment_booking, container, false
         )
-
+        binding.root.bookingBtn.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_navigation_booking_to_navigation_payment)
+        }
+        binding.root.bookingBtn.setOnClickListener {view: View ->
+            view.findNavController().navigate(R.id.action_navigation_booking_to_navigation_payment)
+        }
         return binding.root
     }
 }
