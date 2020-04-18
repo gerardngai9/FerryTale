42e84adee224a060da819a9829247441d3c842e6
wweijiann <siewwj-am17@student.tarc.edu.my>
2020-04-19 06:48:52 +0800

    Register and Login commit

--- app/src/main/java/com/example/myapplication/RegisterFragment.kt
+++ app/src/main/java/com/example/myapplication/RegisterFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -11,18 +12,24 @@ import android.widget.Spinner
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.myapplication.databinding.FragmentRegisterBinding
+import android.util.Patterns
+import android.widget.Toast
+import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_register.*
+import kotlinx.android.synthetic.main.fragment_register.view.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class RegisterFragment : Fragment(){
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        auth = FirebaseAuth.getInstance()
         val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
             inflater,
             R.layout.fragment_register, container, false
@@ -39,6 +46,11 @@ class RegisterFragment : Fragment(){
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+        binding.root.Signup.setOnClickListener {
+            signUpUser()
+            //view.findNavController().navigate(R.id.action_navigation_login_to_navigation_home)
+        }
+
 
         /*roleSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {
@@ -51,5 +63,92 @@ class RegisterFragment : Fragment(){
 
         }*/
         return binding.root
+    }
+
+    private fun signUpUser() {
+
+        if (name.text.toString().isEmpty()) {
+            name.error = "Name cannot be Empty"
+            name.requestFocus()
+            return
+        }
+
+        if (birthday.text.toString().isEmpty()) {
+            birthday.error = "Birthday cannot be Empty"
+            birthday.requestFocus()
+            return
+        }
+
+        if (phone.text.toString().isEmpty()) {
+            phone.error = "Phone No cannot be Empty"
+            phone.requestFocus()
+            return
+        }
+
+        if (email.text.toString().isEmpty()) {
+            email.error = "Email cannot be Empty"
+            email.requestFocus()
+            return
+        }
+
+
+
+
+
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+            email.error = "Please enter valid email"
+            email.requestFocus()
+            return
+        }
+
+        if (editText8.text.toString().isEmpty()) {
+            editText8.error = "Password cannot be Empty"
+            editText8.requestFocus()
+            return
+        }
+        if (editText9.text.toString().isEmpty()) {
+            editText9.error = "Please Retype Password"
+            editText9.requestFocus()
+            return
+        }
+
+        if(editText8.text.toString() != editText9.text.toString())
+        {
+            editText9.error = "Please Enter same password"
+            editText9.requestFocus()
+            return
+        }
+
+
+        auth.createUserWithEmailAndPassword(email.text.toString(), editText8.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    user?.sendEmailVerification()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val ref = FirebaseDatabase.getInstance().getReference("Customers")
+
+                                val customerID = ref.push().key
+                                val customer = Customer(customerID.toString(), name.text.toString(), birthday.text.toString(), phone.text.toString(), email.text.toString(), roleSpinner.selectedItem.toString())
+                                ref.child(customerID.toString()).setValue(customer).addOnCompleteListener{
+                                    Toast.makeText(activity, "Customer save successfully", Toast.LENGTH_LONG).show()
+
+                                }
+
+                                view?.findNavController()?.navigate(R.id.action_navigation_register_to_navigation_login)
+//                                val intent = Intent(activity, LoginFragment::class.java)
+//                                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+//                                startActivity(intent)
+                            }
+                        }
+                } else {
+                    Toast.makeText(
+                        activity, "Sign Up failed. Try again after some time.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
